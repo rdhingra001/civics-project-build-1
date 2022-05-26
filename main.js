f5b6e4ca837c05720bc81adf50fa5f39505109b8
Ronit Dhingra <ronit@Dhingra-Family-iMac.local>
2022-05-25 20:24:01 -0400

    fixed deprecated server init code and deleted mongodb code; will transition to sql database (hopefully) - ronit

--- main.js
+++ main.js
@@ -6,8 +6,6 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const dotenv = require('dotenv');
 const path = require('path');
-const mongoose = require('mongoose');
-const flash = require('express-flash');
 const session = require('express-session');
 // Create important variables
 const app = express(); // Our app instance
@@ -20,15 +18,6 @@ const chatRouter = require('./routes/chat/chat.js');
 // Configure our environment variables
 dotenv.config();
 
-// Connect to our database
-mongoose.connect(
-    process.env.DB_URI,
-    { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true },
-    () => {
-    console.log('Connected to Buddiey DB!');
-    }
-);
-
 // Express Middleware
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -37,12 +26,6 @@ app.use(cookieParser());
 app.use('/auth', authRouter);
 app.use('/chat', chatRouter);
 app.use(express.static("public"));
-app.use(flash());
-app.use(session({
-    secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: false
-}));
 
 app.get('/', (req, res) => res.render('chat/chat'));
 
